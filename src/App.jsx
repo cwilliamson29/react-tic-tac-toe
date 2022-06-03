49c169b94f5f7a952ef59e820085817b81ba7b24
Chris Williamson <chriswill0629@gmail.com>
2022-06-03 08:34:41 -0400

    Git initial commite after nano-react-app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>Welcome to React Vite Micro App!</h1>
-      <p>Hard to get more minimal than this React app.asdfasdf</p>
+      <p>Hard to get more minimal than this React app.</p>
     </div>
   );
 }
